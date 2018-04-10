507cefcf1f0300807c283a254898620d4c1f9140
Daniil <daniilprohorov@gmail.com>
2018-04-10 21:45:30 +0300

    Add files via upload

---
+++ log.h
@@ -0,0 +1,14 @@
+#ifndef LOG_H
+#define LOG_H
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+ofstream logfile("log.txt", ios_base::app);
+
+
+#endif // LOG_H
